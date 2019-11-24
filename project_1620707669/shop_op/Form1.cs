03bd0919a07a02e81db76f4a6c49d3e2f931ca12
RemuRemu <59070187@it.kmitl.ac.th>
2019-11-24 14:44:29 +0700

    fix bug and add update

--- project_1620707669/shop_op/Form1.cs
+++ project_1620707669/shop_op/Form1.cs
@@ -18,12 +18,12 @@ namespace shop_op
         }
         private SqlConnection connection;
         private DataSet dataSt;
-        String constr = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\Database1.mdf;Integrated Security=True";
+        string constr = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\Database1.mdf;Integrated Security=True";
 
         private void Form1_Load(object sender, EventArgs e)
         {
             connection = new SqlConnection(constr);
-            connection.Open();
+            
         }
         private void button1_Click(object sender, EventArgs e)
         {
@@ -32,32 +32,41 @@ namespace shop_op
             string g_name = textBox2.Text;
             double g_price = Convert.ToDouble(textBox3.Text);
             int g_unit_multip = Convert.ToInt32(numericUpDown1.Value);
-            String g_unit = textBox4.Text;
+            string g_unit = textBox4.Text;
             int g_quantity = Convert.ToInt32(textBox5.Text);
             SelectData(g_id);
-           if (dataSt.Tables["check"] == null)
+           if (dataSt.Tables["check"].Rows.Count == 0)
             {
-                MessageBox.Show("checkA");
-                /*String stmt2 = "insert into Goods VALUES (@g_id,@g_name, @g_price, @g_unit_multip , @g_unit,@g_quantity);";
-                SqlCommand cm = new SqlCommand(stmt2, connection);
+                
+                string stmt2 = "insert into Goods (Serial_no,g_name,g_price,g_unitnum,g_unit,g_quantity) VALUES (@g_id,@g_name, @g_price, @g_unit_multip , @g_unit,@g_quantity);";
+                SqlCommand cm2 = new SqlCommand(stmt2,connection);
 
-                cm.Parameters.Clear();
-                cm.Parameters.AddWithValue("g_id", g_id);
-                cm.Parameters.AddWithValue("g_name", g_name);
-                cm.Parameters.AddWithValue("g_price", g_price);
-                cm.Parameters.AddWithValue("g_unit_multip", g_unit_multip);
-                cm.Parameters.AddWithValue("g_unit", g_unit);
-                cm.Parameters.AddWithValue("g_quantity", g_quantity);
-                cm.ExecuteNonQuery();
-                MessageBox.Show("Yatta!");*/
+                
+                cm2.Parameters.AddWithValue("g_id", g_id);
+                cm2.Parameters.AddWithValue("g_name", g_name);
+                cm2.Parameters.AddWithValue("g_price", g_price);
+                cm2.Parameters.AddWithValue("g_unit_multip", g_unit_multip);
+                cm2.Parameters.AddWithValue("g_unit", g_unit);
+                cm2.Parameters.AddWithValue("g_quantity", g_quantity);
+                cm2.ExecuteNonQuery();
+                connection.Close();
+                MessageBox.Show(g_id+g_name+g_quantity);
             }
             else {
-                MessageBox.Show("checkb");
+                int db_quantity = Convert.ToInt32(dataSt.Tables["check"].Rows[0]["g_quantity"]);
+                int new_quantity = db_quantity + g_quantity;
+                string stmt2 = "UPDATE Goods SET g_quantity = @new_quantity WHERE Serial_no = @g_id;";
+                SqlCommand cm2 = new SqlCommand(stmt2, connection);
+                cm2.Parameters.Clear();
+                cm2.Parameters.AddWithValue("new_quantity", new_quantity);
+                cm2.Parameters.AddWithValue("g_id", g_id);
+                cm2.ExecuteNonQuery();
+                connection.Close();
             }
         }
         private void SelectData(String g_id){
-            
-            String stmt = "Select * from Goods where Serial_no = @g_id";
+            connection.Open();
+            string stmt = "Select * from Goods where Serial_no = @g_id";
             SqlCommand cm = new SqlCommand(stmt, connection);
             SqlDataAdapter adapter = new SqlDataAdapter(cm);
             cm.Parameters.Clear();
@@ -65,6 +74,7 @@ namespace shop_op
             cm.ExecuteNonQuery();
             dataSt = new DataSet();
             adapter.Fill(dataSt, "check");
+            
         }
 
        
