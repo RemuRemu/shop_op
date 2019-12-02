213b90c929132b7ac388f8dab7096ec5ad7cad37
RemuRemu <59070187@it.kmitl.ac.th>
2019-12-02 10:35:36 +0700

    add Customer form

--- project_1620707669/shop_op/Form3.cs
+++ project_1620707669/shop_op/Form3.cs
@@ -7,36 +7,59 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.Data.SqlClient;
 namespace shop_op
 {
     public partial class Form3 : Form
     {
+        private SqlConnection connection;
+        private DataSet dataSt;
         public Form3()
         {
             InitializeComponent();
         }
 
-        private void radioButton3_CheckedChanged(object sender, EventArgs e)
-        {
-            if (radioButton3.Checked)
-            {
-                textBox4.Enabled = true;
-            }
-            else {
-                textBox4.Enabled = false;
-            }
-        }
 
         private void button1_Click(object sender, EventArgs e)
         {
+            String c_no = textBox4.Text;
             String c_name = textBox1.Text;
             String c_address = textBox2.Text;
             String c_email = textBox3.Text;
             String c_phone = textBox5.Text;
-            if (radioButton1.Checked) { String c_person = radioButton1.Text; }
-            else if (radioButton2.Checked) { String c_person = radioButton2.Text; }
-            else if (radioButton3.Checked) { String c_person = textBox4.Text; }
+            findCustomers(c_no);
+            if (dataSt.Tables["customers"].Rows.Count == 0) {
+                string stmt = "INSERT INTO Customers VALUES(@c_no,@c_name,@c_address,@c_email,@c_phone,0)";
+                SqlCommand cm = new SqlCommand(stmt,connection);
+                cm.Parameters.AddWithValue("c_no",c_no);
+                cm.Parameters.AddWithValue("c_name", c_name);
+                cm.Parameters.AddWithValue("c_address", c_address);
+                cm.Parameters.AddWithValue("c_email", c_email);
+                cm.Parameters.AddWithValue("c_phone", c_phone);
+                cm.ExecuteNonQuery();
+                MessageBox.Show("สมัครสมาชิกใหม่เรียบร้อยแล้ว");
+            }
+            else { MessageBox.Show("รหัสบัตรนี้มีผู้ใช้งานแล้ว"); }
+            
+        }
+
+        private void Form3_Load(object sender, EventArgs e)
+        {
+            string constr = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\Database1.mdf;Integrated Security=True";
+            connection = new SqlConnection(constr);
+            connection.Open();
+        }
+        private void findCustomers(string c_no)
+        {
+            string stmt = "SELECT * FROM Customers where c_no = @customers or c_phone = @customers";
+            SqlCommand cm = new SqlCommand(stmt, connection);
+            SqlDataAdapter adapter = new SqlDataAdapter(cm);
+            cm.Parameters.Clear();
+            cm.Parameters.AddWithValue("@customers", c_no);
+            cm.ExecuteNonQuery();
+            dataSt = new DataSet();
+            adapter.Fill(dataSt, "customers");
+
         }
     }
 }
--- project_1620707669/shop_op/Form4.Designer.cs
+++ project_1620707669/shop_op/Form4.Designer.cs
@@ -40,11 +40,11 @@
             this.button2 = new System.Windows.Forms.Button();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.label3 = new System.Windows.Forms.Label();
-            this.button3 = new System.Windows.Forms.Button();
-            this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.SuspendLayout();
@@ -125,6 +125,7 @@
             // 
             // listBox1
             // 
+            this.listBox1.Enabled = false;
             this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
             this.listBox1.FormattingEnabled = true;
             this.listBox1.ItemHeight = 24;
@@ -170,7 +171,7 @@
             this.groupBox1.Size = new System.Drawing.Size(985, 429);
             this.groupBox1.TabIndex = 10;
             this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "groupBox1";
+            this.groupBox1.Text = "รายการชำระสินค้า";
             // 
             // groupBox2
             // 
@@ -184,23 +185,25 @@
             this.groupBox2.Size = new System.Drawing.Size(981, 182);
             this.groupBox2.TabIndex = 11;
             this.groupBox2.TabStop = false;
-            this.groupBox2.Text = "groupBox2";
+            this.groupBox2.Text = "สมาชิก";
             // 
-            // textBox2
+            // label5
             // 
-            this.textBox2.Location = new System.Drawing.Point(166, 34);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(166, 20);
-            this.textBox2.TabIndex = 0;
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label5.Location = new System.Drawing.Point(21, 132);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 20);
+            this.label5.TabIndex = 12;
             // 
-            // label3
+            // label4
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(22, 37);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(128, 13);
-            this.label3.TabIndex = 1;
-            this.label3.Text = "เบอร์โทรศัพท์/รหัสสมาชิก";
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label4.Location = new System.Drawing.Point(21, 78);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 20);
+            this.label4.TabIndex = 11;
             // 
             // button3
             // 
@@ -213,25 +216,21 @@
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.Button3_Click);
             // 
-            // label4
+            // label3
             // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.label4.Location = new System.Drawing.Point(21, 78);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(51, 20);
-            this.label4.TabIndex = 11;
-            this.label4.Text = "label4";
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(22, 37);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(128, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "เบอร์โทรศัพท์/รหัสสมาชิก";
             // 
-            // label5
+            // textBox2
             // 
-            this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.label5.Location = new System.Drawing.Point(21, 132);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(51, 20);
-            this.label5.TabIndex = 12;
-            this.label5.Text = "label5";
+            this.textBox2.Location = new System.Drawing.Point(166, 34);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(166, 20);
+            this.textBox2.TabIndex = 0;
             // 
             // Form4
             // 
